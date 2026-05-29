d9e2e95907708ea693b18c23cb1eb572841b8e26
ldmanh23 <ld.manh237@gmail.com>
2026-05-29 13:42:10 +0700

    a

--- Assets/0_Game/Scripts/Manager/IngameManager.cs
+++ Assets/0_Game/Scripts/Manager/IngameManager.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IngameManager : MonoBehaviour
+public class IngameManager : SingletonMonoBehaviour<IngameManager>
 {
     public LevelSO curLevel;
 
